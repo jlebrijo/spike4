78ec94d00de7557af6324b47a18e6ee47dbc08a5
Juan Lebrijo <juan@lebrijo.com>
2023-02-12 21:39:25 +0100

    devise config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'devise/registrations#edit', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
 end
